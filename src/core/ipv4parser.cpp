f6c4f4e5afc10916aad3ac6b7cc8376c002d4b5d
Mat3o <matlagit@outlook.com>
2020-03-27 10:36:57 +0100

    refactoring and keep the convention

--- src/core/ipv4parser.cpp
+++ src/core/ipv4parser.cpp
@@ -1,10 +1,13 @@
 #include "ipv4parser.h"
 
+#include <bitset>
+#include <string>
+
 namespace core {
     std::bitset<32> ipv4parser::stringDecimal2octets(std::string IP)
     {
         std::bitset<32> octetContainer;
-            for(int i = 0; i < 4; i++)
+            for(short i = 0; i < 4; i++)
             {
                 octetContainer <<= 8;
                 auto position = IP.find_first_of('.');
@@ -18,9 +21,8 @@ namespace core {
     std::string ipv4parser::octetsToString(const std::bitset<32>& octetsIP)
     {
         auto stringBinary = octetsIP.to_string();
-        stringBinary.insert(stringBinary.begin()+ 8, '.');
-        stringBinary.insert(stringBinary.begin()+ 17, '.');
-        stringBinary.insert(stringBinary.begin()+ 26, '.');
+        for(short i = 8; i <= 26; i+=9)
+            stringBinary.insert(stringBinary.begin()+ i, '.');
         return stringBinary;
     }
 }
